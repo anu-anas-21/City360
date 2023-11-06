6efdcba4659f8e2c00a8b931cf656619830459d3
anas-anu <sng22mca55@sngce.ac.in>
2023-11-06 09:57:59 +0530

    Commit 3

--- app/src/main/java/com/example/city360/Home.java
+++ app/src/main/java/com/example/city360/Home.java
@@ -3,12 +3,17 @@ package com.example.city360;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        txt=findViewById(R.id.main);
     }
 }
